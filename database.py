0a925fa94371ae24a03867845169f3e6ad2b2fa7
noman asim <asimnoman30@gmail.com>
2026-09-15 16:28:33 +0500

    chore: initialize FastAPI project and configure database

---
+++ database.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+	raise RuntimeError("DATABASE_URL is not set. Add it to a local .env file.")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+	pass
