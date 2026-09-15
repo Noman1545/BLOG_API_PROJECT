3e25db03f89ebbb135a0164b0a02c6c844e09bf5
noman asim <asimnoman30@gmail.com>
2026-09-16 04:35:25 +0500

    fix: add blog creation endpoint

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy.orm import Session
+from database import engine,SessionLocal
 import models
+import schemas
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+# Home route
 @app.get("/")
 def home():
     return {"message": "BLOG API started"}
+
+# Create Blog
+@app.post("/create",response_model=schemas.BlogRespose)
+def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
+    new_blog=models.Blog(
+        title=blog.title,
+        content=blog.content)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+    
---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+# input schema
+class BlogCreate(BaseModel):
+    title:str
+    content:str
+    
+# output schema
+class BlogRespose(BaseModel):
+    id:int
+    title:str
+    content:str
+
+    class Config:
+        from_attributes = True
+    
