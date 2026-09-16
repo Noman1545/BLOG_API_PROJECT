e5a89fb7fecfc5d8da15864e0131dc37de65290d
noman asim <asimnoman30@gmail.com>
2026-09-16 05:25:03 +0500

    feat: add blog CRUD endpoints

--- main.py
+++ main.py
@@ -34,5 +34,30 @@ def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
 @app.get("/blogs", response_model=list[schemas.BlogRespose])
 def get_blogs(db:Session=Depends(get_db)):
     return db.query(models.Blog).all()
+
+@app.get("/blogs/{blog_id}",response_model=schemas.BlogRespose)
+def get_blog(blog_id:int,db:Session=Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==blog_id).first()
+    if not blog:
+        raise HTTPException(status_code=404,detail="blog not found")
+    return blog
+
+@app.put("/edit/{id}",response_model=schemas.BlogRespose)
+def edit_blog(id:int,blog:schemas.BlogCreate,db:Session=Depends(get_db)):
+    existing_blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not existing_blog:
+        raise HTTPException(status_code=404,detail="invalid id number")
+    existing_blog.content=blog.content
+    existing_blog.title=blog.title
     
-    
+    db.commit()
+    db.refresh(existing_blog)
+    return existing_blog
+@app.delete("/delete/{id}")
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=404,detail="invalid id")
+    db.delete(blog)
+    db.commit()
+    return {"message":"BLOG deleted successfully"}
